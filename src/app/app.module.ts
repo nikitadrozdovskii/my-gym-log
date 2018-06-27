94c29d8f794c46b03512a8a551bea181da8d76be
nikitadrozdovskii <butch386@gmail.com>
2018-06-26 21:09:22 -0500

    Added ExeDetailComponent with hardcoded exercise details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExeDetailComponent } from './exe-detail/exe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExeDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/exe-detail/exe-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exe-detail',
+  templateUrl: './exe-detail.component.html',
+  styleUrls: ['./exe-detail.component.css']
+})
+export class ExeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
