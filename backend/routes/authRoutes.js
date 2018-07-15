b6c6bbbafb018cd302846ba0ce49dc2bd133b48a
nikitadrozdovskii <butch386@gmail.com>
2018-07-15 15:03:35 -0500

    added User model, now authRoutes middleware creates new users in users collection, validating email uniqueness and encrypting the password

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require('../models/user');
+
 
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-    console.log(req.body);
+    console.log(req.body.password);
+    bcrypt.hash(req.body.password, 10).then((hash) => {
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+        user.save().then(() => {
+            res.status(201).json({
+                message: "User successfully created"
+            });
+        }).catch((error) => {
+            res.status(500).json({
+                error:error
+            });
+        });;
+    });
 });
 
 module.exports = router;
