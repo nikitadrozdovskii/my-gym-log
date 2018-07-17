67b5df0edbbd120ce7b65c3257ca1585623ee51a
nikitadrozdovskii <butch386@gmail.com>
2018-07-17 14:53:45 -0500

    added frontend logic to get dates array and max weights array from HTTP returned object for future plotting

--- src/app/analytics/analytics.service.ts
+++ src/app/analytics/analytics.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class AnalyticsService implements OnInit {
+    fetchedExeData: Array<any>;
+    dates;
+    maxWeights;
 
 
     constructor(private http: HttpClient) {}
@@ -10,10 +13,38 @@ export class AnalyticsService implements OnInit {
 
     }
 
-    getMaxWeightData(exeName: string){
+    getExeData(exeName: string){
         this.http.get(`http://localhost:3000/api/analytics/${exeName}`)
         .subscribe((res: {results:Array<any>})=>{
-            console.log(res);
+            this.fetchedExeData = res.results;
+            this.getDatesArray();
+            this.getMaxWeightsArray();
         });
+    }
+
+    getDatesArray () {
+        const datesArray = this.fetchedExeData.map((day) => {
+            return Object.keys(day)[0];
+        });
+        this.dates = datesArray;
+    }
+
+    getMaxWeightsArray () {
+        console.log(this.fetchedExeData);
+        const setsArray = this.fetchedExeData.map((day) => {
+            const tempDay = JSON.parse(JSON.stringify(day));
+            const sets = Object.values(tempDay)[0][0].sets;
+            //sets mapped to leave only weights
+            const mappedSets = sets.map((set) => {
+                return +set.weight;
+            });
+            return mappedSets;
+        });
+        console.log(setsArray);
+        //sets with only weights mapped to find max weights
+        const setsArrayMax = setsArray.map((set: Array<number>) => {
+            return Math.max.apply(Math, set);
+        });
+        this.maxWeights = setsArrayMax;
     }
 }
