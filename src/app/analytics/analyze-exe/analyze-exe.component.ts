10b15231b10073fcc5a33cfe35b7da07c28f4029
nikitadrozdovskii <butch386@gmail.com>
2018-07-17 08:46:54 -0500

    Added Chart.js to packages, harcoded simple line graph to analyze-exe component

--- src/app/analytics/analyze-exe/analyze-exe.component.ts
+++ src/app/analytics/analyze-exe/analyze-exe.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-analyze-exe',
   templateUrl: './analyze-exe.component.html',
   styleUrls: ['./analyze-exe.component.css']
 })
-export class AnalyzeExeComponent implements OnInit {
+export class AnalyzeExeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
+  canvas;
+  ctx;
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    let chart = new Chart(this.ctx, {
+      "type":"line",
+      "data":{"labels":["January","February","March","April","May","June","July"],
+      "datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],
+      "fill":false,
+      "borderColor":"rgb(75, 192, 192)",
+      "lineTension":0.1}]},
+      "options":{}});
+  }
+
+    
+  
 }
