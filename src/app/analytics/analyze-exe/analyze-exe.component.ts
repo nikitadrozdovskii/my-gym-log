b800f079bb133ea4b515162cdb6c024633a39d55
nikitadrozdovskii <butch386@gmail.com>
2018-07-18 05:38:53 -0500

    Added input validation for exe1 input in AnalyzeExe component, the name of the line in the graph now comes from input

--- src/app/analytics/analyze-exe/analyze-exe.component.ts
+++ src/app/analytics/analyze-exe/analyze-exe.component.ts
@@ -10,6 +10,7 @@ import { AnalyticsService } from '../analytics.service';
 export class AnalyzeExeComponent implements OnInit, AfterViewInit {
 
   constructor(private anService: AnalyticsService) { }
+  errorMessage: string;
   exe1: string;
   canvas;
   ctx;
@@ -23,6 +24,8 @@ export class AnalyzeExeComponent implements OnInit, AfterViewInit {
   }
 
   onSubmit() {
+    this.dates = null;
+    this.weights = null;
     this.plot();
   }
 
@@ -30,12 +33,19 @@ export class AnalyzeExeComponent implements OnInit, AfterViewInit {
     this.anService.getExeData(this.exe1).then(()=>{;
       this.dates = this.anService.getDates();
       this.weights = this.anService.getWeights();
+      //if there were no results
+      if (this.dates.length===0) {
+        this.errorMessage = "No results for given exercise, please make sure you entered the name correctly.";
+        setTimeout(() => {
+          this.errorMessage = null;
+        },3500);
+      }
       this.canvas = document.getElementById('myChart');
       this.ctx = this.canvas.getContext('2d');
       let chart = new Chart(this.ctx, {
         "type":"line",
         "data":{"labels":this.dates,
-        "datasets":[{"label":"Max Weight","data":this.weights,
+        "datasets":[{"label":this.exe1,"data":this.weights,
         "fill":false,
         "borderColor":"rgb(75, 192, 192)",
         "lineTension":0.1}]},
