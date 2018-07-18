57684ccf8ab53618a504f28e1e1a8e21b5c21103
nikitadrozdovskii <butch386@gmail.com>
2018-07-18 07:47:21 -0500

    If entered exe in AnalyzeExe component does not yield any matches in database, it is not plotted and error message is displayed

--- src/app/analytics/analyze-exe/analyze-exe.component.ts
+++ src/app/analytics/analyze-exe/analyze-exe.component.ts
@@ -32,10 +32,16 @@ export class AnalyzeExeComponent implements OnInit, AfterViewInit {
     if (this.exeCounter === 0){
       this.anService.getExeData(formName.value).then(()=>{
         const dates = this.anService.getDates();
+        if (dates.length===0) {
+          this.errorMessage = "No results for given exercise, please make sure you entered the name correctly.";
+          setTimeout(() => {
+            this.errorMessage = null;
+          },3500);
+          return;
+        }
         const weights = this.anService.getWeights();
         const name = formName.value;
         this.plot(dates, weights, name);
-        this.exeCounter++;
       });
     } else {
       this.anService.getExeData(formName.value).then(()=>{
@@ -51,12 +57,12 @@ export class AnalyzeExeComponent implements OnInit, AfterViewInit {
 
   plot(dates: Array<string>, weights: Array<number>, name: string){
       //if there were no results
-      if (dates.length===0) {
-        this.errorMessage = "No results for given exercise, please make sure you entered the name correctly.";
-        setTimeout(() => {
-          this.errorMessage = null;
-        },3500);
-      }
+      // if (dates.length===0) {
+      //   this.errorMessage = "No results for given exercise, please make sure you entered the name correctly.";
+      //   setTimeout(() => {
+      //     this.errorMessage = null;
+      //   },3500);
+      // }
       this.canvas = document.getElementById('myChart');
       this.ctx = this.canvas.getContext('2d');
       this.chart = new Chart(this.ctx, {
@@ -64,7 +70,7 @@ export class AnalyzeExeComponent implements OnInit, AfterViewInit {
         "data":{"labels":dates,
         "datasets":[{"label":name,"data":weights,
         "fill":false,
-        "borderColor":"rgb(75, 192, 192)",
+        "borderColor":this.colorScheme[this.exeCounter%5],
         "lineTension":0.1}]},
         "options":{
           scales: {
@@ -74,13 +80,23 @@ export class AnalyzeExeComponent implements OnInit, AfterViewInit {
             }]
           }
         }});
+        console.log(this.exeCounter);
+
+        this.exeCounter++;
     // this.addData(["2018-07-24", "2018-07-28"], [80,90], "Biceps");
     // this.addData(["2018-07-28", "2018-08-01", "2018-08-05"], [200,250,260], "Legs");
   }
 
   addData(labels: Array<string>, data: Array<number>, name: string) {
+    //if no exes found, do not plot, display error message
+    if (labels.length===0) {
+      this.errorMessage = "No results for given exercise, please make sure you entered the name correctly.";
+      setTimeout(() => {
+        this.errorMessage = null;
+      },3500);
+      return;
+    }
     //for each label in existing lables array, push null onto the new dataarray
-    this.exeCounter++;
     const newDataArray = [];
     this.chart.data.labels.forEach(() => {newDataArray.push(null)});
     data.forEach((d) => {
@@ -91,9 +107,11 @@ export class AnalyzeExeComponent implements OnInit, AfterViewInit {
       this.chart.data.labels.push(label);
     });
 
-    this.chart.data.datasets.push({"label": name, "data": newDataArray, "fill":false, "borderColor": this.colorScheme[this.exeCounter]});
+    this.chart.data.datasets.push({"label": name, "data": newDataArray, "fill":false, "borderColor": this.colorScheme[this.exeCounter%5]});
     this.chart.update();
-    
+    console.log(this.exeCounter);
+    this.exeCounter++;
+        
 }
   
 }
