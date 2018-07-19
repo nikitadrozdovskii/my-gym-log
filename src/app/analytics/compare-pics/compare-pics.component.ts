29d4aecb2b50765c3f2596cab06603fea9f504a4
nikitadrozdovskii <butch386@gmail.com>
2018-07-19 06:40:57 -0500

    Added compare progresss pictures functionality

--- src/app/analytics/compare-pics/compare-pics.component.ts
+++ src/app/analytics/compare-pics/compare-pics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExeService } from '../../exe.service';
 
 @Component({
   selector: 'app-compare-pics',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compare-pics.component.css']
 })
 export class ComparePicsComponent implements OnInit {
+  imageUrl1;
+  imageUrl2;
+  date1;
+  date2;
 
-  constructor() { }
+  constructor(private exeService: ExeService) { }
 
   ngOnInit() {
+  }
+
+  fetchPicture(event){
+    const date = event.target.value;
+    const inputNum = event.target.id;
+    this.exeService.getImageFromServer(date);
+    const subscription = this.exeService.imageUpdated.subscribe((imagePath) => {
+      if (+inputNum === 1){
+        this.date1 = date;
+        this.imageUrl1 = imagePath;
+      } else {
+        this.imageUrl2 = imagePath;
+        this.date2 = date;
+      }
+      subscription.unsubscribe();
+    });
+
   }
 
 }
